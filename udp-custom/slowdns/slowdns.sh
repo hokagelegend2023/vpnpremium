c668935a5778324afcd810ad707a6b03c7229504
hokage <142867374+hokage2023@users.noreply.github.com>
2023-08-29 16:01:51 +0700

    Update slowdns.sh

--- udp-custom/slowdns/slowdns.sh
+++ udp-custom/slowdns/slowdns.sh
@@ -22,7 +22,7 @@ rm nsdomain
 
 read -rp "Masukkan Subdomain Yang Dipakai Host Sekarang: " -e sub
 SUB_DOMAIN=${sub}
-NS_DOMAIN=ns-${SUB_DOMAIN}
+NS_DOMAIN=ns.${SUB_DOMAIN}
 echo $NS_DOMAIN > /root/nsdomain
 
 apt update -y
