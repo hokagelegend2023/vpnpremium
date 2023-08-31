c1580a63cbc219a1e9f6a42a6f1e0481d95f1e63
hokage <142867374+hokage2023@users.noreply.github.com>
2023-08-31 10:59:29 +0700

    Update updatemenu.sh

--- update/updatemenu.sh
+++ update/updatemenu.sh
@@ -15,11 +15,9 @@ rm -rf add-tr
 
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/menu/menu.sh" 
-wget -O updatemenu "https://raw.githubusercontent.com/hokagelegend2023/vpnpremim/main/update/updatemenu.sh" 
-wget -O menutheme "https://raw.githubusercontent.com/hokagelegend2023/vpnpremim/main/update/menutheme.sh"
-wget -O backup "https://raw.githubusercontent.com/hokagelegend2023/vpnpremim/main/backup/backup.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/hokagelegend2023/vpnpremim/main/backup/menu-backup.sh"
-wget -O running "https://raw.githubusercontent.com/hokagelegend2023/vpnpremim/main/update/running.sh"
+wget -O updatemenu "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/updatemenu.sh" 
+wget -O menutheme "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/menutheme.sh"
+wget -O running "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/running.sh"
 wget -O info "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/info.sh" 
 wget -O vpnpremium "https://raw.githubusercontent.com/hokagelegend2023/update/main/vpnpremium"
 wget -O l2tpmenu "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/menu/l2tpmenu.sh"
@@ -29,34 +27,14 @@ wget -O add-tr "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/x
 
 chmod +x info
 chmod +x vpnpremium
-chmod +x restart
-chmod +x addssh
-chmod +x grpcmenu2
-chmod +x grpc2
-chmod +x grpcupdate2
 chmod +x menu
 chmod +x menutheme
 chmod +x menuinfo
-chmod +x ipsaya
-chmod +x sl-fix
-chmod +x sshovpnmenu
-chmod +x ssmenu
-chmod +x ssrmenu
-chmod +x vmessmenu
-chmod +x vlessmenu
-chmod +x grpcmenu
-chmod +x grpcupdate
-chmod +x trmenu
-chmod +x trgomenu
-chmod +x setmenu
-chmod +x slowdnsmenu
 chmod +x running
 chmod +x updatemenu
 chmod +x setmenu
-chmod +x backup
-chmod +x menu-backup
 chmod +x add-tr
-sl-download-info
+
 #install-sldns
 #install-ss-plugin
 #xray-grpc
