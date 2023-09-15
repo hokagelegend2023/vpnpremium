768be68cbf7c5505725c3848d0ec05422d462d61
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-15 14:45:03 +0700

    Update addwg.sh

--- wireguard/addwg.sh
+++ wireguard/addwg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -25,7 +24,7 @@ fi
 clear
 # Load params
 source /etc/wireguard/params
-source /var/lib/crot/ipvps.conf
+source /var/lib/hokagevpn-pro/ipvps.conf
 if [[ "$IP" = "" ]]; then
 SERVER_PUB_IP=$(wget -qO- ipinfo.io/ip);
 else
